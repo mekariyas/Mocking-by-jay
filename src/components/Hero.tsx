d04eb2da6d99b7a0ec663bbd465862ccea5d9f03
Mekariyas <mekariyas05@gmail.com>
2025-09-26 19:20:52 +0300

    added framer animation to the landing page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,21 +1,34 @@
 import { useNavigate} from "react-router-dom"
-
+import { motion } from "motion/react"
 import HeroImg from "../assets/hero.webp"
 
 const Hero = () => {
   const navigate = useNavigate()
   return (
     <section className="w-full h-[87vh] relative">
-      <section className="w-full h-full absolute pt-6 linear backdrop-grayscale backdrop-brightness-50 flex flex-col items-center">
-                <h1 className="w-full h-20 text-4xl text-red-600 font-extrabold text-center">Mocking By Jay</h1>
-                <p className="text-white w-[80%] md:w-[60%] mt-8 text-wrap text-xl font-semibold">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac vestibulum metus, in rhoncus augue. Vivamus dictum blandit erat, eget consequat erat. Nullam at dolor a justo vehicula viverra.</p>
-                <section className="w-[80%] md:w-[60%]  h-[20vh] flex justify-center  items-center gap-6 mt-10">
-                  <button className="w-[25%] h-[45%] text-white font-bold text-xl bg-red-600 rounded-md cursor-pointer" onClick={()=>navigate("/bookArtist")}>Book Artist</button>
-                  <button className="w-[25%] h-[45%] text-white font-bold text-xl bg-black  rounded-md cursor-pointer" onClick={()=>navigate("/about")}>See More</button>
+      <section className="w-full h-full absolute pt-6 linear backdrop-grayscale backdrop-brightness-50 flex flex-col items-center overflow-hidden">
+                <motion.h1 className="w-full h-20 text-4xl text-red-600 font-extrabold text-center" 
+                initial={{scale:0, opacity: 0}} 
+                whileInView={{scale: 1, opacity: 1}} 
+                transition={{ease:"easeOut", duration:1, delay: 0.4}}>Mocking By Jay</motion.h1>
+                <motion.p className="text-white w-[80%] md:w-[60%] mt-8 text-wrap text-xl font-semibold"
+                initial={{x:200, opacity:0}} whileInView={{x: 0, opacity: 1}} transition={{ease:"easeOut", duration:2, delay: 0.75}}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac vestibulum metus, in rhoncus augue. Vivamus dictum blandit erat, eget consequat erat. Nullam at dolor a justo vehicula viverra.
+                </motion.p>
+                <section className="w-[80%] md:w-[60%]  h-[20vh] flex justify-center  items-center gap-8 mt-10">
+                  <motion.button 
+                  className="w-[25%] h-[45%] text-white font-bold text-xl bg-red-600 rounded-md cursor-pointer transition transition-discrete duration-200 delay-100 ease-in-out hover:scale-125" 
+                  onClick={()=>navigate("/bookArtist")}
+                  initial={{scale:0 , opacity:0}} whileInView={{scale: 1, opacity:1}} transition={{ease: "easeOut", duration:2, delay:0.5}}>Book Artist</motion.button>
+                  <motion.button 
+                  className="w-[25%] h-[45%] text-white font-bold text-xl bg-black  rounded-md cursor-pointer transition transition-discrete duration-200 delay-100 ease-in-out hover:scale-125" 
+                  onClick={()=>navigate("/about")}
+                  initial={{scale:0, opacity: 0}} whileInView={{scale: 1, opacity: 1}} transition={{ease: "easeOut", duration:2, delay:0.65}}
+                  >See More</motion.button>
                 </section>
                 
       </section>
-        <img src={HeroImg} alt="Hero-image" className="w-full h-full" loading="lazy" />
+        <img src={HeroImg} alt="Hero-image" className="w-full h-full" loading="lazy"/>
     </section>
   )
 }
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom"
+import { motion } from "motion/react"
 import Hero from "../components/Hero"
 import Artist1 from "../assets/artist.webp"
 import Artist2 from "../assets/artist-2.webp"
@@ -10,32 +11,61 @@ const Index = ()=>{
     return(
         <>
             <Hero/>
-            <section className="bg-black text-red w-full pt-2 flex flex-col items-center">
-                <h1 className="w-full h-20 text-3xl font-bold text-red-600 text-center pt-6">Our Artists</h1>
-                <ul className="w-full flex gap-2 flex flex-col items-center md:flex-row md:justify-around mb-10">
-                    <li key={1} className="w-[60%] md:w-[30%] h-[55vh]">
-                        <img src={Artist1} alt="artist-1" className="w-full h-[90%]"/>
+            <section className="bg-black text-red w-full pt-2 flex flex-col items-center overflow-hidden">
+                <motion.h1 
+                className="w-full h-20 text-3xl font-bold text-red-600 text-center pt-6"
+                initial={{x:-1000 ,opacity:0}} whileInView={{x:0, opacity:1}} transition={{ease:"easeOut",duration:2, delay:0.85}}>Our Artists</motion.h1>
+                <ul className="w-full gap-2 flex flex-col items-center md:flex-row md:justify-around mb-10">
+                    <motion.li key={1} 
+                    className="w-[60%] md:w-[30%] h-[55vh]"
+                    initial={{opacity: 0}}
+                    whileInView={{opacity:1}}
+                    transition={{ease:"easeOut", delay:0.5, duration:2}}>
+                        <motion.img src={Artist1} alt="artist-1" 
+                        className="w-full h-[90%]"
+                        initial={{opacity:0}}
+                        animate={{opacity:1}}/>
                         <p className="bg-red-600 text-white text-center">Artist 1</p>
-                    </li>
-                    <li key={2} className="w-[60%] md:w-[30%] h-[55vh]">
-                        <img src={Artist2} alt="artist-2" className="w-full h-[90%]"/>
+                    </motion.li>
+                    <motion.li key={2} 
+                    className="w-[60%] md:w-[30%] h-[55vh]"
+                    initial={{opacity:0}}
+                    whileInView={{opacity:1}}
+                    transition={{ease: "easeOut", delay:0.75, duration:2}}>
+                        <motion.img src={Artist2} alt="artist-2" className="w-full h-[90%]"/>
                         <p className="bg-red-600 text-white text-center">Artist 2</p>
-                    </li>
-                    <li key={3} className="w-[60%] md:w-[30%] h-[55vh]">
-                        <img src={Artist3} alt="artist-3" className="w-full h-[90%]"/>
+                    </motion.li>
+                    <motion.li key={3} 
+                    className="w-[60%] md:w-[30%] h-[55vh]"
+                    initial={{opacity: 0}}
+                    whileInView={{opacity:1}}
+                    transition={{ease:"easeOut", delay:1, duration:2}}>
+                        <motion.img src={Artist3} alt="artist-3" 
+                        className="w-full h-[90%]"
+                        initial={{opacity:0}}
+                        animate={{opacity:1}}/>
                         <p className="bg-red-600 text-white text-center">Artist 3</p>
-                    </li>
+                    </motion.li>
                 </ul>
             </section>
             <hr className="w-full h-1 bg-red-600" />
-            <section className="bg-black text-red w-full pt-2 flex flex-col items-center">
-                <h1 className="w-full h-20 text-3xl font-bold text-red-600 text-center pt-6">Book A Session</h1>
-                <p className="text-white md:w-[80%] text-lg text-wrap pl-2 pr-2 md:pl-0 md:pr-2 mb-8">
+            <section className="bg-black text-red w-full pt-2 flex flex-col items-center overflow-hidden">
+                <motion.h1 
+                initial={{x:-1000 ,opacity:0}} whileInView={{x:0, opacity:1}} transition={{ease:"easeOut",duration:1, delay:0.5}}
+                className="w-full h-20 text-3xl font-bold text-red-600 text-center pt-6">Book A Session</motion.h1>
+                <motion.p 
+                initial={{x:200, opacity:0}} whileInView={{x: 0, opacity: 1}} transition={{ease:"easeOut", duration:2, delay: 0.75}} 
+                className="text-white md:w-[80%] text-lg text-wrap pl-2 pr-2 md:pl-0 md:pr-2 mb-8">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac vestibulum metus, in rhoncus augue. Vivamus dictum blandit erat, eget consequat erat.
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac vestibulum metus, in rhoncus augue. Vivamus dictum blandit erat, eget consequat erat.
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac vestibulum metus, in rhoncus augue. Vivamus dictum blandit erat, eget consequat erat.
-                </p>
-                <button className="cursor-pointer text-white border-red-600 border-[1px] rounded-md mb-8 w-40 h-16 font-bold translate-all duration-200 delay-50 ease-in-out hover:bg-red-600" onClick={()=>navigate("/contactUs")}>Book A Session</button>
+                </motion.p>
+                <motion.button 
+                initial={{scale:0, opacity:0}}
+                whileInView={{scale: 1, opacity: 1}} 
+                transition={{ease:"easeOut", duration:2, delay: 0.5}}
+                className="cursor-pointer text-white border-red-600 border-[1px]  rounded-md mb-8 w-40 h-16 font-bold transition transition-discrete duration-200 delay-100 ease-in-out hover:scale-125 hover:bg-red-600" 
+                onClick={()=>navigate("/contactUs")}>Book A Session</motion.button>
             </section>
         </>
     )
